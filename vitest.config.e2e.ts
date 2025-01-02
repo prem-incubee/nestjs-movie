c5bb343f05a40f6fbb69000b8147dd4dbb2a9bde
Akshay Vadher <akshay@incubyte.co>
2025-01-02 19:24:52 +0530

    test vitest seed

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,24 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+    root: './',
+    deps: {
+      interopDefault: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [swc.vite()],
+});
