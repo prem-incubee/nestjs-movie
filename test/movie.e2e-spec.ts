4d3b64b95660f318ae64dc91bb2d93b77ed22a88
premg <prem@incubyte.co>
2025-01-06 10:46:23 +0530

    rating feature

--- test/movie.e2e-spec.ts
+++ test/movie.e2e-spec.ts
@@ -59,7 +59,7 @@ describe('movie aggregator for profitability', () => {
         status: 200,
         body: {
           data: {
-            money: {made: 10, budget: 9}
+            money: { made: 10, budget: 9 },
           },
         },
       },
@@ -82,6 +82,46 @@ describe('movie aggregator for profitability', () => {
     );
     expect(reseponse.status).toBe(200);
     expect(reseponse.body).toBeDefined();
-    expect(reseponse.body.profitable).toBe("PROFITABLE");
+    expect(reseponse.body.profitable).toBe('PROFITABLE');
+  });
+});
+
+describe('movie aggregator for rating', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    await mock.start(3001);
+  });
+  beforeEach(async () => {
+    mock.addInteraction({
+      request: { method: 'GET', path: '/movies/batman' },
+      response: {
+        status: 200,
+        body: {
+          data: {
+            rating: 8,
+          },
+        },
+      },
+    });
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+  afterAll(async () => {
+    await mock.stop();
+  });
+
+  it('given a movie name, get the rating of the movie', async () => {
+    const movieName = 'batman';
+    const reseponse = await request(app.getHttpServer()).get(
+      `/movies/${movieName}/rating`,
+    );
+    expect(reseponse.status).toBe(200);
+    expect(reseponse.body).toBeDefined();
+    expect(reseponse.body.rating).toBe(4);
   });
 });
